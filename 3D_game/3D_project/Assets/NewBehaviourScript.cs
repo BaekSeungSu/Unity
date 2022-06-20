ecd2549421440d5bb61d9958131b87608674a248
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2022-06-20 20:31:20 +0900

    ver1

---
+++ 3D_game/3D_project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Unity!");
+
+        //1.변수
+        int level = 5;
+        float strength = 15.5f;
+        string playerName = "baek";
+        bool isFullLevel = false;
+
+        //2. 그룹형 변수
+        string[] monsters = { "슬라임", "사막뱀", "악마" };
+        int[] monsterLevel = new int[3];
+        monsterLevel[0] = 1;
+        monsterLevel[1] = 6;
+        monsterLevel[2] = 20;
+
+        Debug.Log("맵에 존재하는 몬스터");
+        Debug.Log(monsters[0]);
+        Debug.Log(monsters[1]);
+        Debug.Log(monsters[2]);
+        Debug.Log("맵에 존재하는 몬스터의 레벨");
+        Debug.Log(monsterLevel[0]);
+        Debug.Log(monsterLevel[1]);
+        Debug.Log(monsterLevel[2]);
+
+        /*Debug.Log("용사의 이름은?");
+          Debug.Log(playerName);
+          Debug.Log("용사의 레벨은?");
+          Debug.Log(level);
+          Debug.Log("용사의 힘은?");
+          Debug.Log(strength);
+          Debug.Log("용사는 만렙인가?");
+          Debug.Log(isFullLevel);*/
+
+
+        List<string> items = new List<string>();
+        items.Add("HP포션30");
+        items.Add("MP포션30");
+
+        Debug.Log("가지고 있는 아이템");
+        Debug.Log(items[0]);
+        Debug.Log(items[1]);
+
+        items.RemoveAt(0);
+        Debug.Log("가지고 있는 아이템");
+        Debug.Log(items[0]);
+        Debug.Log(items[1]);
+
+    }
+
+}
